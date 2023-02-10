72597c59a3ffdf79bee6a45bd8ef414be5df37ee
BlackOokami <aramarza@mail.ru>
2023-02-10 15:03:24 +0300

    Initial Commit

---
+++ SurfApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SurfApp
+//
+//  Created by ブラック狼 on 10.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var image: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
