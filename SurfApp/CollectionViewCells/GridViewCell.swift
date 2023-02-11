3436baaa8e847eb72f98ca146d2d2d1c548fea07
BlackOokami <aramarza@mail.ru>
2023-02-12 00:18:38 +0300

    Ups second collectionVIew

---
+++ SurfApp/CollectionViewCells/GridViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  GridViewCell.swift
+//  SurfApp
+//
+//  Created by ブラック狼 on 11.02.2023.
+//
+
+import UIKit
+
+class GridViewCollectionCell: UICollectionViewCell {
+    @IBOutlet weak var label: UILabel!
+}
--- SurfApp/VIewControllers/ViewController + Extension.swift
+++ SurfApp/VIewControllers/ViewController + Extension.swift
@@ -10,17 +10,30 @@ import UIKit
 extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return rowsInView
+        if collectionView == collectionView.viewWithTag(1) {
+            return rowsInView
+        } else {
+            return rowsInView
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CollectionViewCell
-        let index = indexPath.row % list.count
-        cell.label.text = list[index]
-        cell.backgroundColor = UIColor(named: "BackgroundButtonColor")
-        cell.layer.cornerRadius = cell.bounds.height * 0.12
-        return cell
+        if collectionView == collectionView.viewWithTag(1) {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GridCell", for: indexPath) as! GridViewCollectionCell
+            let index = indexPath.row % list.count
+            cell.label.text = list[index]
+            cell.backgroundColor = UIColor(named: "BackgroundButtonColor")
+            cell.layer.cornerRadius = cell.bounds.height * 0.20
+            return cell
+        } else {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CollectionViewCell
+            let index = indexPath.row % list.count
+            cell.label.text = list[index]
+            cell.backgroundColor = UIColor(named: "BackgroundButtonColor")
+            cell.layer.cornerRadius = cell.bounds.height * 0.20
+            return cell
+        }
     }
     
-    
 }
+
